b78b26ae01240061489f3075eda126692bf9e3d8
suki <suki@suki-MBP-01.local>
2019-11-06 19:56:58 +0900

    init

---
+++ precache-manifest.369813488992434fa475112171b0c315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3bc0ab54fe8b484bfe11aa51fcbf76",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "c9239b5c48c6da652c2b",
+    "url": "/avatar/static/css/main.7aee2ad4.chunk.css"
+  },
+  {
+    "revision": "54c8655f4822151593c1",
+    "url": "/avatar/static/js/2.24006aa4.chunk.js"
+  },
+  {
+    "revision": "c9239b5c48c6da652c2b",
+    "url": "/avatar/static/js/main.34699a83.chunk.js"
+  },
+  {
+    "revision": "b55dc1516cc8c65eb2c0",
+    "url": "/avatar/static/js/runtime-main.22b54ed0.js"
+  },
+  {
+    "revision": "56aef99aca80bdbff263cafbb31f541e",
+    "url": "/avatar/static/media/body.56aef99a.png"
+  },
+  {
+    "revision": "d1f8f1e698e03456530f205fef4fde14",
+    "url": "/avatar/static/media/tanbata.d1f8f1e6.png"
+  }
+]);
